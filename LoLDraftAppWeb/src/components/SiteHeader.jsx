258f52b24caa67124d2c7919f98f7d00c83c1c97
niickzl <20051024nick@gmail.com>
2025-08-17 02:28:40 -0700

    Add EasyChart external to WebHub

--- LoLDraftAppWeb/src/components/SiteHeader.jsx
+++ LoLDraftAppWeb/src/components/SiteHeader.jsx
@@ -13,7 +13,7 @@ export default function SiteHeader() {
       }}
     >
       <a
-        href="/"
+        href="./index.html"
         style={{
           display: "flex",
           alignItems: "center",
