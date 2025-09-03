e3fc8554847536f7eb6fb2e822ce9379ffd41778
niickzl <20051024nick@gmail.com>
2025-09-03 12:12:16 -0700

    Revert "Attempt to fix pathing issue"
    This reverts commit a9a947842d9deb871710356bdf035ffb81255d06.

--- LoLDraftAppWeb/vite.config.js
+++ LoLDraftAppWeb/vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/LoLDraftAppWeb/dist/',
+  base: '/LoLDraftAppWeb/',
   plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
   },
-  server: {
-    port: 3000
-  }
 })
