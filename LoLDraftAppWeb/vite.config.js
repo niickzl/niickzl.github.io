ef3d9a2f53ba98b85e3f14c44005224001d837a3
niickzl <20051024nick@gmail.com>
2025-09-03 12:11:53 -0700

    Revert "Attempt fix"
    This reverts commit 55c5f7c7de38913b7cee5688d7c406d96fcbd9af.

--- LoLDraftAppWeb/vite.config.js
+++ LoLDraftAppWeb/vite.config.js
@@ -1,27 +1,8 @@
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   base: "/LoLDraftAppWeb/",
-  plugins: [react()],
   build: {
-    outDir: "dist",
-    sourcemap: true,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
-  },
-  server: {
-    port: 3000,
-    open: true,
-    host: true
-  },
-  preview: {
-    port: 3000,
-    open: true
+    outDir: "dist"
   }
 });
