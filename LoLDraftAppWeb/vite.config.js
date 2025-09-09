e77a2f172a36f954990f39e2045b4c32603a1905
niickzl <20051024nick@gmail.com>
2025-09-09 04:01:25 -0700

    Update fix to LoL Web

--- LoLDraftAppWeb/vite.config.js
+++ LoLDraftAppWeb/vite.config.js
@@ -6,7 +6,7 @@ import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/LolDraftAppWebDist/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
